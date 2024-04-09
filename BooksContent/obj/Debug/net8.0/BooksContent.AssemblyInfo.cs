84446fedf9912603d829bf8ad9cf59a0921fc07a
Paula Carolina Misas Marin <111092976+pmisas@users.noreply.github.com>
2024-04-09 01:27:47 -0500

    fix problems

--- BooksContent/obj/Debug/net8.0/BooksContent.AssemblyInfo.cs
+++ BooksContent/obj/Debug/net8.0/BooksContent.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksContent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aee9f890372fc4de57601ef305eccb57bc94051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksContent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksContent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SocialInteraction/obj/Debug/net8.0/SocialInteraction.AssemblyInfo.cs
+++ SocialInteraction/obj/Debug/net8.0/SocialInteraction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialInteraction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4aee9f890372fc4de57601ef305eccb57bc94051")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialInteraction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialInteraction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
